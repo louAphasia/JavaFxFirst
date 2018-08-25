f0f287c52f9f1eb910af1d203c9acb06b039966c
louAphasia <lousalome23@gmail.com>
2018-08-25 19:16:48 +0200

    drawing lines end 2408

--- src/drawingLinesExample/DrawingLines.java
+++ src/drawingLinesExample/DrawingLines.java
@@ -6,14 +6,16 @@
 package drawingLinesExample;
 
 import javafx.application.Application;
-import javafx.beans.value.ObservableValue;
+
 import javafx.scene.*;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.control.Slider;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.shape.Arc;
+import javafx.scene.text.Font;
 
 import java.util.Random;
 /**
@@ -28,9 +30,9 @@ public class DrawingLines extends Application {
          primaryStage.setTitle("Rysuj linie");
          
          Group root = new Group();
-         Scene scene = new Scene(root, 400, 300, Color.TURQUOISE);
+         Scene scene = new Scene(root, 500, 600, Color.TURQUOISE);
          
-         Line vline = new Line(10,10,400,10);
+         Line vline = new Line(10,10,500,10);
          
          vline.setStroke(Color.VIOLET);
          vline.setStrokeWidth(15);
@@ -41,7 +43,7 @@ public class DrawingLines extends Application {
          
          root.getChildren().add(vline);
          
-         Line white= new Line(10,30,400,30);
+         Line white= new Line(10,30,500,30);
          
           white.setStroke(Color.WHITE);
          white.setStrokeWidth(15);
@@ -62,13 +64,16 @@ public class DrawingLines extends Application {
         
         root.getChildren().add(slider);
         
-        Text offsettext= new Text("Stroke dash "+ slider.getValue());
+        Text offsettext= new Text("votes NO FOX fur PROTEST "+ slider.getValue());
         offsettext.setX(15);
-        offsettext.setY(80);
+        offsettext.setY(300);
+        offsettext.setStrokeWidth(2);
+        Font ft=Font.font("arial",25);
+           offsettext.setFont(ft);
         offsettext.setStroke(Color.PLUM);
         
         slider.valueProperty().addListener((ov, curVal, newVal)-> {
-            offsettext.setText("Stroke dash 2"+ newVal);
+            offsettext.setText("votes NO FOX fur PROTEST "+ newVal);
          });
         
         root.getChildren().add(offsettext);
@@ -77,44 +82,62 @@ public class DrawingLines extends Application {
         path.setStrokeWidth(3);
         
         MoveTo moveto= new MoveTo();
-        moveto.setX(100);
-        moveto.setY(50);
+        moveto.setX(170);
+        moveto.setY(380);
         
         QuadCurveTo quadcurve = new QuadCurveTo();
-        quadcurve.setX(150);
-        quadcurve.setY(150);
-        quadcurve.setControlX(100);
-        quadcurve.setControlY(50);
+        quadcurve.setX(280);
+        quadcurve.setY(380);
+        quadcurve.setControlX(225);
+        quadcurve.setControlY(520);
         
-        LineTo linel= new LineTo();
-        linel.setX(200);
-        linel.setY(100);
+        LineTo line1= new LineTo();
+        line1.setX(280);
+        line1.setY(380);
         
-        LineTo linep= new LineTo();
-        linep.setX(100);
-        linep.setY(150);
+        LineTo line2= new LineTo();
+        line2.setX(260);
+        line2.setY(340);
         
-        path.getElements().addAll(moveto, quadcurve, linel, linep);
+         LineTo line3= new LineTo();
+        line3.setX(225);
+        line3.setY(380);
+        
+         LineTo line4= new LineTo();
+        line4.setX(190);
+        line4.setY(340);
+        
+         LineTo line5= new LineTo();
+        line5.setX(170);
+        line5.setY(380);
+        
+         LineTo line6= new LineTo();
+        line6.setX(280);
+        line6.setY(380);
+        
+        path.getElements().addAll(moveto, quadcurve, line1, line2, line3,line4,line5,line6);
         
         path.setTranslateY(30);
         
         path.setStroke(Color.AZURE);
-        path.setFill(Color.AQUA);
+        path.setFill(Color.CORAL);
         
        
        root.getChildren().add(path);
         //RAND TEXT 
        
        Random rand = new Random (System.currentTimeMillis());
-       for(int i=0; i<100;i++) {
-           int x=rand.nextInt((int)300);
-           int y= rand.nextInt((int)250);
-           int blue=rand.nextInt(255);
+       for(int i=0; i<20;i++) {
+           int x=rand.nextInt((int)400);
+           int y= rand.nextInt((int)500);
            
-           Text text = new Text(x,y,"Ice ice");
            
-           int rot= rand.nextInt(360);
-           text.setFill(Color.BISQUE);
+           Text text = new Text(x,y,"lucky one ANIMAL RIGHTS movement");
+           Font font=Font.font("serif",20);
+           text.setFont(font);
+           
+           int rot= rand.nextInt(40);
+           text.setFill(Color.ANTIQUEWHITE);
            text.setRotate(rot);
            
            root.getChildren().add(text);
