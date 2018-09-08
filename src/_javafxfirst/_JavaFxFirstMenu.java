f65549c02e2b6030eff1a33693050510d35c25ca
louAphasia <lousalome23@gmail.com>
2018-09-08 21:59:05 +0200

    new commit menu click 0409

--- src/_javafxfirst/_JavaFxFirstMenu.java
+++ src/_javafxfirst/_JavaFxFirstMenu.java
@@ -6,14 +6,19 @@
 package _javafxfirst;
 
 
-import javafx.scene.paint.Color;
+import javafx.collections.ObservableList;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
@@ -21,6 +26,8 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.MouseButton;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 
 /**
@@ -35,8 +42,75 @@ public class _JavaFxFirstMenu extends Application {
         primaryStage.setTitle("menu");
         BorderPane root=new BorderPane();
         
-        Scene scene= new Scene(root, 300, 200,Color.BLUE);
+        Scene scene= new Scene(root, 500, 350);
+    
+scene.setFill(Color.BLANCHEDALMOND);
+root.setBackground(Background.EMPTY);
+
+
+        ///background color???
         
+        //new function
+        GridPane grid= new GridPane();
+        grid.setPadding(new Insets(10));
+        
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPrefHeight(Double.MAX_VALUE);
+        ColumnConstraints col1= new ColumnConstraints(150,150,Double.MAX_VALUE);
+        ColumnConstraints col2= new ColumnConstraints(50);
+        ColumnConstraints col3=new ColumnConstraints(150,150,Double.MAX_VALUE);
+        col1.setHgrow(Priority.ALWAYS);
+        col3.setHgrow(Priority.ALWAYS);
+        grid.getColumnConstraints().addAll(col1, col2, col3);
+        
+        Label cand = new Label("Candidate");
+        GridPane.setHalignment(cand, HPos.CENTER);
+        grid.add(cand,0,0);
+        
+        Label okregi= new Label("confirmed");
+        grid.add(okregi,2,0);
+        GridPane.setHalignment(okregi, HPos.CENTER);
+        
+        ObservableList<String> candidates= FXCollections.observableArrayList("anna kowalska", "jerzy lusowski", "pola negri", "paulina koss", "nadiezna slakowsky");
+        ListView<String> candlist= new ListView<>(candidates);
+        grid.add(candlist, 0, 1);
+        
+        ObservableList<String> okregiName= FXCollections.observableArrayList();
+        ListView<String> okregilist= new ListView(okregiName);
+        grid.add(okregilist, 2, 1);
+        
+        Button rb= new Button(">");
+        rb.setOnAction((ActionEvent event)->{
+            String potential= candlist.getSelectionModel().getSelectedItem();
+            if (potential != null){
+                candlist.getSelectionModel().clearSelection();
+                candidates.remove(potential);
+                okregiName.add(potential);
+                }    
+        });
+        
+        Button lb= new Button ("<");
+        lb.setOnAction((ActionEvent event)-> {
+            String notokreg= okregilist.getSelectionModel().getSelectedItem();
+            if(notokreg!=null){
+                okregilist.getSelectionModel().clearSelection();;
+            okregiName.remove(notokreg);
+            candidates.add(notokreg);
+            }
+        });
+        
+        VBox vb= new VBox(5);
+        vb.getChildren().addAll(rb,lb);
+        vb.setAlignment(Pos.CENTER);
+        grid.add(vb, 1,1);
+        
+        root.setCenter(grid);
+        
+        GridPane.setVgrow(root,Priority.ALWAYS);
+        
+        
+      
         MenuBar menub= new MenuBar();
         root.setTop(menub);
         
